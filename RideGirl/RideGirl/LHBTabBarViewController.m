67ca57d1b553a2f8d11726f88741b50713fa86f9
LHB <lhb@LHBdeMacBook-Pro.local>
2016-05-31 23:08:29 +0800

    通过appearance属性统一修改tabor 文字的颜色和字体大小

--- RideGirl/RideGirl/LHBTabBarViewController.m
+++ RideGirl/RideGirl/LHBTabBarViewController.m
@@ -22,6 +22,44 @@
     vc01.tabBarItem.title = @"精华";
     vc01.tabBarItem.image = [UIImage imageNamed:@"tabBar_essence_icon"];
     vc01.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_essence_click_icon"];
+    // 改变系统tabbar 选中图片的的渲染模式   第一种做法
+//    //一、系统默认会把选中图片渲染成蓝色,可以更改渲染模式，来不让系统渲染
+//    UIImage *image = [UIImage imageNamed:@"tabBar_essence_click_icon"];
+//    //一直保持原始的图片
+//    image = [image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+//    vc01.tabBarItem.selectedImage = image;
+    
+    //第二种做法:在Assets.xcassets中对不要渲染的图片的属性做更改:render as 
+
+    
+    
+//    改变系统tabbar 文字的属性 ,其实是改变vc tabbarItem 的属性
+    //第一种做法 更改文字属性
+//    NSMutableDictionary *normalDic = [NSMutableDictionary dictionary];
+//    normalDic[NSFontAttributeName] = [UIFont systemFontOfSize:10];
+//    normalDic[NSForegroundColorAttributeName] = [UIColor darkGrayColor];
+//    [vc01.tabBarItem setTitleTextAttributes:normalDic forState:UIControlStateNormal];
+//    
+//    NSMutableDictionary *selectDic = [NSMutableDictionary dictionary];
+//    selectDic[NSFontAttributeName] = [UIFont systemFontOfSize:10];
+//    selectDic[NSForegroundColorAttributeName] = [UIColor orangeColor];
+//    [vc01.tabBarItem setTitleTextAttributes:selectDic forState:UIControlStateSelected];
+    
+    //第二种做法 ： 拿到tabBarItem的ppearance的外观、皮肤对象，并对它设置，这样所有tabBarItem的属性都一样了(使用条件：发现某个方法后面有 UI_APPEARANCE_SELECTOR 的宏)
+    
+    NSMutableDictionary *normalDic = [NSMutableDictionary dictionary];
+    normalDic[NSFontAttributeName] = [UIFont systemFontOfSize:10];
+    normalDic[NSForegroundColorAttributeName] = [UIColor lightGrayColor];
+    
+    NSMutableDictionary *selectDic = [NSMutableDictionary dictionary];
+    selectDic[NSFontAttributeName] = [UIFont systemFontOfSize:10];
+    selectDic[NSForegroundColorAttributeName] = [UIColor darkGrayColor];
+
+
+    UITabBarItem *item = [UITabBarItem appearance];
+    [item setTitleTextAttributes:normalDic forState:UIControlStateNormal];
+    [item setTitleTextAttributes:selectDic forState:UIControlStateSelected];
+    
     [self addChildViewController:vc01];
     
     
