969755951cf5f549bbb09273518a30a1713b842c
LHB <lhb@LHBdeMacBook-Pro.local>
2016-05-31 21:50:48 +0800

    v 1.0.0 160531 2150 新建git工程

---
+++ RideGirl/RideGirl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RideGirl
+//
+//  Created by LHB on 16/5/31.
+//  Copyright © 2016年 LHB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RideGirl/RideGirl/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  RideGirl
+//
+//  Created by LHB on 16/5/31.
+//  Copyright © 2016年 LHB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
