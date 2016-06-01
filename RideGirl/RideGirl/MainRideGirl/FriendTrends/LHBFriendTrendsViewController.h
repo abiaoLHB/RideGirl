61d9199ebc5d840bf8ac3c93a738a15b79b8a042
LHB <lhb@LHBdeMacBook-Pro.local>
2016-06-01 21:26:04 +0800

    关注模块未登录

--- RideGirl/RideGirl/MainRideGirl/FriendTrends/LHBFriendTrendsViewController.h
+++ RideGirl/RideGirl/MainRideGirl/FriendTrends/LHBFriendTrendsViewController.h
@@ -9,5 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface LHBFriendTrendsViewController : UIViewController
-
+/*
+    在xib中设置文字换行时，要按着option＋回车键才能换行。代码中直接拼接\n就能换行
+ 
+    xib时要设置xib的filesOner，并且把filesOner的view连给xib中view
+ */
 @end
